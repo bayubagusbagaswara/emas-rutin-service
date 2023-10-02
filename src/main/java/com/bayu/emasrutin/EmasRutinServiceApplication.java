0ee3e034cc9cc8b043616c4daf55597de8be2542
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-10-02 19:44:02 +0700

    Initial commit

---
+++ src/main/java/com/bayu/emasrutin/EmasRutinServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bayu.emasrutin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmasRutinServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmasRutinServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bayu/emasrutin/EmasRutinServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bayu.emasrutin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmasRutinServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
